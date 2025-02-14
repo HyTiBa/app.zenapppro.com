7ec14d7ad8acaae93a731ec29dc3bbccdfdccd71
Bao Huynh <124868331+HyTiBa@users.noreply.github.com>
2025-02-14 15:02:17 +0700

    lint

--- next.config.ts
+++ next.config.ts
@@ -3,25 +3,29 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: "export",
   // Can be safely removed in newer versions of Next.js
-  // future: {
+  future: {
 
-  //   // by default, if you customize webpack config, they switch back to version 4.
-  //   // Looks like backward compatibility approach.
-  //   webpack5: true,   
-  // },
+    // by default, if you customize webpack config, they switch back to version 4.
+    // Looks like backward compatibility approach.
+    webpack5: true,   
+  },
 
-  // webpack(config) {
-  //   config.resolve.fallback = {
+  webpack(config) {
+    config.resolve.fallback = {
 
-  //     // if you miss it, all the other options in fallback, specified
-  //     // by next.js will be dropped.
-  //     ...config.resolve.fallback,  
+      // if you miss it, all the other options in fallback, specified
+      // by next.js will be dropped.
+      ...config.resolve.fallback,  
 
-  //     fs: false, // the solution
-  //   };
+      fs: false, // the solution
+    };
     
-  //   return config;
-  // },
+    return config;
+  },
+  reactStrictMode:true,
+eslint:{
+  ignoreDuringBuilds:true
+}
 };
 
 export default nextConfig;
